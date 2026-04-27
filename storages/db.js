05e791656028f6fddb3b03e717a0eef9fcd45127
SDHINSA <sohanad@icloud.com>
2026-04-27 15:11:25 -0600

    Deploying the app

--- storages/db.js
+++ storages/db.js
@@ -5,12 +5,20 @@ dotenv.config();
 
 // All of the following properties should be read from environment variables
 // We're hardcoding them here for simplicity
+// const pool = new Pool({
+//   host: process.env.HOST, // or wherever the db is hosted // was "localhost"
+//   user: process.env.ROLE_NAME,                              // was "sohanadhinsa"
+//   database: process.env.DB,                             // was "messages"
+//   password: process.env.ROLE_PW,                               // was "1234567"
+//   port: process.env.DB_PORT // The default port                     // was 5432
+// });
+
 const pool = new Pool({
-  host: process.env.HOST, // or wherever the db is hosted // was "localhost"
-  user: process.env.ROLE_NAME,                              // was "sohanadhinsa"
-  database: process.env.DB,                             // was "messages"
-  password: process.env.ROLE_PW,                               // was "1234567"
-  port: process.env.DB_PORT // The default port                     // was 5432
+  connectionString: process.env.DATABASE_URL,
+  ssl:
+    process.env.NODE_ENV === "production"
+      ? { rejectUnauthorized: false }
+      : false,
 });
 
 export default pool;
